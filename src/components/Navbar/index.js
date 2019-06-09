713dc680b8331e35b04bd1cfb9d5acf26c9530d1
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-06-09 19:04:11 -0400

    added styling and structure to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './style.css';
 
 export class index extends Component {
   render() {
     return (
       <div>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-primary'>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav mr-auto'>
               <li className='nav-item active'>
@@ -17,8 +18,7 @@ export class index extends Component {
               <li className='nav-item'>
                 <button>
                   <a className='nav-link' href='#'>
-                    <i class='fab fa-trello' />
-                    Boards
+                    <i class='fab fa-trello' /> Boards
                   </a>
                 </button>
               </li>
@@ -38,63 +38,161 @@ export class index extends Component {
                   </button>
                 </form>
               </li>
-              <li className='nav-item dropdown'>
-                <a
-                  className='nav-link dropdown-toggle'
-                  href='#'
-                  id='navbarDropdown'
-                  role='button'
-                  data-toggle='dropdown'
-                  aria-haspopup='true'
-                  aria-expanded='false'
-                >
-                  Dropdown
-                </a>
-                <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
-                  <a className='dropdown-item' href='#'>
-                    Action
+              {/* <li class='nav-item dropdown'>
+                <button>
+                  <a
+                    class='nav-link'
+                    href='#'
+                    id='navbarDropdown'
+                    role='button'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
+                    Dropdown
                   </a>
-                  <a className='dropdown-item' href='#'>
-                    Another action
-                  </a>
-                  <div className='dropdown-divider' />
-                  <a className='dropdown-item' href='#'>
-                    Something else here
-                  </a>
-                </div>
-              </li>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <a class='dropdown-item' href='#'>
+                      Action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Another action
+                    </a>
+                    <div class='dropdown-divider' />
+                    <a class='dropdown-item' href='#'>
+                      Something else here
+                    </a>
+                  </div>
+                </button>
+              </li> */}
               <li>
                 <a className='navbar-brand' href='#'>
-                  <i class='fab fa-trello' />
-                  Trello
+                  <i class='fab fa-trello' /> Trello
                 </a>
               </li>
-              <li className='nav-item'>
+              <li class='nav-item dropdown'>
                 <button>
-                  <a className='nav-link create' href='#'>
+                  <a
+                    class='nav-link'
+                    href='#'
+                    id='navbarDropdown'
+                    role='button'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
                     <i class='fas fa-plus' />
                   </a>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <h6> Create</h6>
+                    <div class='dropdown-divider' />
+                    <a class='dropdown-item' href='#'>
+                      <h6>Create Board...</h6>
+                      <p>
+                        A board is made up of cards ordered on lists. Use it to
+                        manage projects, track information, or organize
+                        anything.
+                      </p>
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      <h6>Create Team...</h6>
+                      <p>
+                        A team is a group of boards and people. Use it to
+                        organize your company, side hustle, family, or friends.
+                      </p>
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      <h6>Create Business Team...</h6>
+                      <p>
+                        With Business Class your team has more security,
+                        administrative controls, and unlimited Power-Ups.
+                      </p>
+                    </a>
+                  </div>
                 </button>
               </li>
-              <li className='nav-item'>
+              <li class='nav-item dropdown'>
                 <button>
-                  <a className='nav-link info' href='#'>
+                  <a
+                    class='nav-link'
+                    href='#'
+                    id='navbarDropdown'
+                    role='button'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
                     <i class='fas fa-info-circle' />
                   </a>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <h6>Information</h6>
+                    <div class='dropdown-divider' />
+                    <a class='dropdown-item' href='#'>
+                      Action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Another action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Something else here
+                    </a>
+                  </div>
                 </button>
               </li>
-              <li className='nav-item'>
+              <li class='nav-item dropdown'>
                 <button>
-                  <a className='nav-link notifications' href='#'>
+                  <a
+                    class='nav-link'
+                    href='#'
+                    id='navbarDropdown'
+                    role='button'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
                     <i class='far fa-bell' />
                   </a>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <h6>Notifications</h6>
+                    <div class='dropdown-divider' />
+                    <a class='dropdown-item' href='#'>
+                      Action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Another action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Something else here
+                    </a>
+                  </div>
                 </button>
               </li>
-              <li className='nav-item'>
+              <li class='nav-item dropdown'>
                 <button>
-                  <a className='nav-link user' href='#'>
+                  <a
+                    class='nav-link'
+                    href='#'
+                    id='navbarDropdown'
+                    role='button'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
                     <i class='far fa-user' />
                   </a>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <h6>User</h6>
+                    <div class='dropdown-divider' />
+                    <a class='dropdown-item' href='#'>
+                      Profile
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Another action
+                    </a>
+                    <a class='dropdown-item' href='#'>
+                      Something else here
+                    </a>
+                  </div>
                 </button>
               </li>
             </ul>
