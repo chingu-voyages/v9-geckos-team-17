60e736676e1d965333aaadbff3cd334b262c24c9
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-06-10 19:20:20 -0400

    class => className

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,17 +8,17 @@ export class index extends Component {
         <nav className='navbar navbar-expand-lg navbar-light bg-primary'>
           <div className='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul className='navbar-nav mr-auto'>
-              <li className='nav-item active'>
+              <li className='nav-item active home'>
                 <button>
                   <a className='nav-link' href='#'>
-                    <i class='fas fa-home' />
+                    <i className='fas fa-home' />
                   </a>
                 </button>
               </li>
-              <li class='nav-item dropdown'>
+              <li className='nav-item dropdown boards'>
                 <button>
                   <a
-                    class='nav-link'
+                    className='nav-link'
                     href='#'
                     id='navbarDropdown'
                     role='button'
@@ -26,34 +26,36 @@ export class index extends Component {
                     aria-haspopup='true'
                     aria-expanded='false'
                   >
-                    <i class='fab fa-trello' /> Boards
+                    <i className='fab fa-trello' /> Boards
                   </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-                    <a class='dropdown-item' href='#'>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdown'
+                  >
+                    <a className='dropdown-item' href='#'>
                       <form className='form-inline my-2 my-lg-0'>
                         <input
                           className='form-control mr-sm-2'
                           type='search'
-                          placeholder='Search'
+                          placeholder='Find boards by name...'
                           aria-label='Search'
                         />
                         <button
                           className='btn btn-outline-dark my-2 my-sm-0'
                           type='submit'
-                          text='Find boards by name...'
                         >
                           Search
                         </button>
                       </form>
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       <h6>Create Team...</h6>
                       <p>
                         A team is a group of boards and people. Use it to
                         organize your company, side hustle, family, or friends.
                       </p>
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       <h6>Create Business Team...</h6>
                       <p>
                         With Business Class your team has more security,
@@ -63,7 +65,7 @@ export class index extends Component {
                   </div>
                 </button>
               </li>
-              <li>
+              <li className='search'>
                 <form className='form-inline my-2 my-lg-0'>
                   <input
                     className='form-control mr-sm-2'
@@ -79,15 +81,15 @@ export class index extends Component {
                   </button>
                 </form>
               </li>
-              <li>
+              <li className='trello'>
                 <a className='navbar-brand' href='#'>
-                  <i class='fab fa-trello' /> Trello
+                  <i className='fab fa-trello' /> Trello
                 </a>
               </li>
-              <li class='nav-item dropdown'>
+              <li className='nav-item dropdown create'>
                 <button>
                   <a
-                    class='nav-link'
+                    className='nav-link'
                     href='#'
                     id='navbarDropdown'
                     role='button'
@@ -95,12 +97,15 @@ export class index extends Component {
                     aria-haspopup='true'
                     aria-expanded='false'
                   >
-                    <i class='fas fa-plus' />
+                    <i className='fas fa-plus' />
                   </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdown'
+                  >
                     <h6> Create</h6>
-                    <div class='dropdown-divider' />
-                    <a class='dropdown-item' href='#'>
+                    <div className='dropdown-divider' />
+                    <a className='dropdown-item' href='#'>
                       <h6>Create Board...</h6>
                       <p>
                         A board is made up of cards ordered on lists. Use it to
@@ -108,14 +113,14 @@ export class index extends Component {
                         anything.
                       </p>
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       <h6>Create Team...</h6>
                       <p>
                         A team is a group of boards and people. Use it to
                         organize your company, side hustle, family, or friends.
                       </p>
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       <h6>Create Business Team...</h6>
                       <p>
                         With Business Class your team has more security,
@@ -125,10 +130,10 @@ export class index extends Component {
                   </div>
                 </button>
               </li>
-              <li class='nav-item dropdown'>
+              <li className='nav-item dropdown info'>
                 <button>
                   <a
-                    class='nav-link'
+                    className='nav-link'
                     href='#'
                     id='navbarDropdown'
                     role='button'
@@ -136,27 +141,30 @@ export class index extends Component {
                     aria-haspopup='true'
                     aria-expanded='false'
                   >
-                    <i class='fas fa-info-circle' />
+                    <i className='fas fa-info-circle' />
                   </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdown'
+                  >
                     <h6>Information</h6>
-                    <div class='dropdown-divider' />
-                    <a class='dropdown-item' href='#'>
+                    <div className='dropdown-divider' />
+                    <a className='dropdown-item' href='#'>
                       Action
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Another action
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Something else here
                     </a>
                   </div>
                 </button>
               </li>
-              <li class='nav-item dropdown'>
+              <li className='nav-item dropdown notifications'>
                 <button>
                   <a
-                    class='nav-link'
+                    className='nav-link'
                     href='#'
                     id='navbarDropdown'
                     role='button'
@@ -164,27 +172,30 @@ export class index extends Component {
                     aria-haspopup='true'
                     aria-expanded='false'
                   >
-                    <i class='far fa-bell' />
+                    <i className='far fa-bell' />
                   </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdown'
+                  >
                     <h6>Notifications</h6>
-                    <div class='dropdown-divider' />
-                    <a class='dropdown-item' href='#'>
+                    <div className='dropdown-divider' />
+                    <a className='dropdown-item' href='#'>
                       Action
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Another action
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Something else here
                     </a>
                   </div>
                 </button>
               </li>
-              <li class='nav-item dropdown'>
+              <li className='nav-item dropdown users'>
                 <button>
                   <a
-                    class='nav-link'
+                    className='nav-link'
                     href='#'
                     id='navbarDropdown'
                     role='button'
@@ -192,18 +203,21 @@ export class index extends Component {
                     aria-haspopup='true'
                     aria-expanded='false'
                   >
-                    <i class='far fa-user' />
+                    <i className='far fa-user' />
                   </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdown'
+                  >
                     <h6>User</h6>
-                    <div class='dropdown-divider' />
-                    <a class='dropdown-item' href='#'>
+                    <div className='dropdown-divider' />
+                    <a className='dropdown-item' href='#'>
                       Profile
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Another action
                     </a>
-                    <a class='dropdown-item' href='#'>
+                    <a className='dropdown-item' href='#'>
                       Something else here
                     </a>
                   </div>
