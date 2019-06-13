22bda7981eba2783df3e03b9fcb764746d79d237
Rick <alrick.chalar@gmail.com>
2019-06-12 23:56:03 -0400

    add trello icon to side nav

--- src/SideNav/SideNav.js
+++ src/SideNav/SideNav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavItem from '../NavItem/NavItem'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faColumns,faWaveSquare, faPlus, faSignature} from '@fortawesome/free-solid-svg-icons'
+import { faTrello } from '@fortawesome/free-brands-svg-icons'
+import {  faPlus, faSignature,} from '@fortawesome/free-solid-svg-icons'
 
 const sideNav = (props) =>(
   <>
     <nav className="side-nav">
-       <span className='main-nav-item'><NavItem src ='https://trello.com' navName= 'Boards' icon={<FontAwesomeIcon icon={faColumns} />}/></span>
+       <span className='main-nav-item'><NavItem src ='https://trello.com' navName= 'Boards' icon={<FontAwesomeIcon icon={faTrello} />}/></span>
        <NavItem src ='https://trello.com' navName= 'Home' icon={<FontAwesomeIcon icon={faSignature} />}/>
        <NavItem src ='https://trello.com' navName= 'Teams'icon={<FontAwesomeIcon icon={faPlus} />}/>
     </nav>
