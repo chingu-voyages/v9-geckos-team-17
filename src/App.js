c4d6943051af86aff963dac7dd66e27a031293f4
Rick <alrick.chalar@gmail.com>
2019-06-12 22:59:55 -0400

    add and sytle icons for side nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from '../src/components/Navbar';
 import LandingPage from './LandingPage/LandingPage';
 // import LandingPage from "./pages/LandingPage";
