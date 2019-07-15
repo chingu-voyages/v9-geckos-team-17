1dafc6ca3dd1c77a8c2a0e77e05198fb4370186d
Rick <alrick.chalar@gmail.com>
2019-07-14 21:59:05 -0400

    refactor code

--- src/Card/Card.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const card = (props) =>(
-  <div className="card">
-    <a href={props.src} className={props.className}> {props.cardName}</a>
-  </div>
-)
-
-export default card
--- src/Headings/Headings.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const headings = (props) =>(
-  <div className='headings'>
-    <p><span className='heading-icon icon'> {props.icon}</span>{props.heading}</p>
-  </div>
-)
-
-export default headings
