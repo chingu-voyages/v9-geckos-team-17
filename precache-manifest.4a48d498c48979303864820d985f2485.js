3aaf7cc587232f792bf6e99ccabc07272371bb19
Rick <alrick.chalar@gmail.com>
2019-07-15 18:30:07 -0400

    Updates

---
+++ precache-manifest.4a48d498c48979303864820d985f2485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9da531f65812c892d3b0a81435e493e",
+    "url": "/v9-geckos-team-17-clonello/index.html"
+  },
+  {
+    "revision": "316b38ccdd219bc3c90c",
+    "url": "/v9-geckos-team-17-clonello/static/css/main.5dba721b.chunk.css"
+  },
+  {
+    "revision": "f71ae30c30524cf6aa3b",
+    "url": "/v9-geckos-team-17-clonello/static/js/2.c668f60e.chunk.js"
+  },
+  {
+    "revision": "316b38ccdd219bc3c90c",
+    "url": "/v9-geckos-team-17-clonello/static/js/main.ea58e5a6.chunk.js"
+  },
+  {
+    "revision": "9a3f3d311655a691ed0d",
+    "url": "/v9-geckos-team-17-clonello/static/js/runtime~main.7838a3d6.js"
+  }
+]);
